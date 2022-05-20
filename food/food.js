1241ae2d1aa187857a2e5ecc8e42401f0221977b
王炫晟 <91250441+happy123903@users.noreply.github.com>
2022-05-20 09:36:40 +0800

    Update food.js

--- food/food.js
+++ food/food.js
@@ -18,14 +18,20 @@ function draw() {
         rect(50, 100, 150, 2);
     if (choice == 2)
         rect(50, 150, 200, 2);
+    if (choice == 3)
+        rect(50, 200, 75, 2);
+    if (choice == 4)
+        rect(50, 250, 75, 2);
     text("泡麵", 50, 50);
     text("Ubereat", 50, 100);
     text("Foodpanda", 50, 150);
+    text("牛排",50,200);
+    text("大餐",50,250);
 }
 
 function mousePressed() {
     // choice = (choice+1)%3;
-    choice = int(random(3));
+    choice = int(random(5));
 }
 
 function initializeFields() {
